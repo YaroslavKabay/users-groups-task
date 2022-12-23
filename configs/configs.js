74332562819e98a4d974f91774e922b599a33299
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-12-23 16:17:08 +0200

    init

--- configs/configs.js
+++ configs/configs.js
@@ -3,7 +3,7 @@ module.exports = {
 
     MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/database' ,
 
-    FRONTEND_URL: process.env.FRONTEND_URL || 'google.com',
+    // FRONTEND_URL: process.env.FRONTEND_URL || 'google.com',
     //
     // NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL || 'example@email.com',
     // NO_REPLY_PASSWORD: process.env.NO_REPLY_PASSWORD || 'email password',
